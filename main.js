bd1d4230179991dddbb6c1f6f195d047767ad679
Mitchell Lutzke <mitchell649@hotmail.com>
2013-12-31 07:59:34 -0600

    Terrain - Midpoint displacement, Catmull Rom interpolation

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+function catmullRomInterpolate( P0, P1, P2, P3, u ){
+	var u3 = u * u * u;
+	var u2 = u * u;
+	var f1 = -0.5 * u3 + u2 - 0.5 * u;
+	var f2 =  1.5 * u3 - 2.5 * u2 + 1.0;
+	var f3 = -1.5 * u3 + 2.0 * u2 + 0.5 * u;
+	var f4 =  0.5 * u3 - 0.5 * u2;
+	var x = P0.x * f1 + P1.x * f2 + P2.x * f3 + P3.x * f4;
+	var y = P0.y * f1 + P1.y * f2 + P2.y * f3 + P3.y * f4;
+
+	return new Point(x, y);
+}
+
 var Node = function(dataVal, prevVal, nextVal){
 	this.next = nextVal;
 	this.prev = prevVal;
@@ -6,7 +19,12 @@ var Node = function(dataVal, prevVal, nextVal){
 
 Node.prototype = {
 	toArray: function(){
-		// implement to array here
+		var new_array = [];
+		var curr_node = this;
+		while(curr_node != null){
+			new_array.push(curr_node);
+			curr_node = curr_node.next;
+		}
 	}
 };
 
@@ -59,8 +77,9 @@ var Game = {
     draw_loop_handle: 		null,
     physics_loop_handle: 	null,
     points: {
-    	head: null,		// reference to first point-node of doubly linked list
-    	tail: null
+    	head: 				null,	// reference to first point-node of doubly linked list
+    	tail: 				null,
+    	point_array: 		[]
     },
 
     // public functions
@@ -69,14 +88,23 @@ var Game = {
     		console.log("Generating points");
     		// perform points setup
     		// create first two point nodes
-    		Game.points.head = new Node(new Point(0, 0.5), null, null);
-    		Game.points.head.next = new Node(new Point(1, 0.5), null, null);
-    		Game.points.tail = Game.points.next; // finish setting up head/tail pointers
+    		var headNode = new Node(new Point(0, 0.5), null, null);
+    		var tailNode = new Node(new Point(1, 0.5), null, null);
+
+    		headNode.next = tailNode;
+    		tailNode.prev = headNode;
+
+    		Game.points.head = headNode;
+    		Game.points.tail = tailNode;
+
+    		//Game.points.point_array.push(headNode.data);
+    		//Game.points.point_array.push(tailNode.data);
     	}
 
     	// perform terrain generation loops
-    	var h = 0.5;
-    	var num_iterations = 10;
+    	var h = 0.7;
+    	var num_iterations = 6;				// 6 is enough
+    	var catmull_interpolations  = 1;	// 6 is still enough
     	var rand_range = 1;
 
     	var algorithm_time = performance.now();
@@ -96,8 +124,56 @@ var Game = {
     		rand_range *= Math.pow(2, -h); // adjust the h value to lower next random difference
     	}
 
+
+    	// perform catmull rom interpolation to smooth path
+    	// must run at least 1 time to yield any points for final draw
+    	var curr_node = Game.points.head;
+
+    	while( curr_node != null 
+    		&& curr_node.next != null 
+    		&& curr_node.next.next != null
+    		&& curr_node.next.next.next != null){
+
+			var p0 = curr_node.data;
+			var p1 = curr_node.next.data;
+			var p2 = curr_node.next.next.data;
+			var p3 = curr_node.next.next.next.data;
+
+			for(var itr = 0; itr < catmull_interpolations; itr++){
+				var newPoint = catmullRomInterpolate(p0, p1, p2, p3, itr/catmull_interpolations);
+				Game.points.point_array.push(newPoint);
+			}
+
+			curr_node = curr_node.next;
+    	}
+
+    	// handle end point special case interpolation
+
     	algorithm_time = performance.now() - algorithm_time;
-    	console.log(algorithm_time);
+    	console.log("Algorithm Time: " + algorithm_time.toFixed(2));
+    	console.log("Number of points pre-interpolation: " + Game.points.point_array.length / catmull_interpolations);
+    	console.log("Number of points: " + Game.points.point_array.length);
+
+
+    	/*
+    	 * Array implementation saved for benchmark purposes
+	    	// generate with array, because #yolo
+	    	rand_range = 1;
+	    	algorithm_time = performance.now();
+
+	    	for(var itr = 0; itr < num_iterations; itr++){
+	    		for(var i = 0; i < Game.points.point_array.length-1; i+=2){
+	    			var newPoint = Game.points.point_array[i].Avg(Game.points.point_array[i+1]); // average two points
+	    			newPoint.y += Math.random() * (rand_range - (-rand_range)) + (-rand_range);
+	    			Game.points.point_array.splice(i+1, 0, newPoint);
+	    		}
+	    		rand_range *= Math.pow(2, -h); // adjust the h value to lower next random difference
+	    	}
+
+	    	algorithm_time = performance.now() - algorithm_time;
+	    	console.log("Array Time: " + algorithm_time.toFixed(2));
+    	*/
+
 
     	// initialize game loops
     	//Game.physics_loop_handle = setInterval(Game.Physics, Game.physics_timer);
@@ -119,13 +195,13 @@ var Game = {
     	Game.ctx.fillRect(0,0,1000,800);
     	Game.ctx.beginPath();
 
-    	while(curr !== null && curr.next !== null){
-    		
-    		Game.ctx.strokeStyle = "#000000";
-	      	Game.ctx.moveTo(curr.data.x * 1000, curr.data.y * 100 + 400);
-	      	Game.ctx.lineTo(curr.next.data.x * 1000, curr.next.data.y * 100 + 400);
+    	for(var pos = 0; pos < Game.points.point_array.length-1 && Game.points.point_array.length > 1; pos++){
+    		var curr = Game.points.point_array[pos];
+    		var next = Game.points.point_array[pos+1];
 
-	      	curr = curr.next;
+    		Game.ctx.strokeStyle = "#000000";
+	      	Game.ctx.moveTo(curr.x * 1000, curr.y * 100 + 400);
+	      	Game.ctx.lineTo(next.x * 1000, next.y * 100 + 400);
     	}
 
     	Game.ctx.stroke();
@@ -135,6 +211,8 @@ var Game = {
 
 Game.canvas_tag.focus();
 
-Game.Initialize(); 
-Game.PrintPoints();
+	Game.Initialize(); 
+	Game.PrintPoints();
+
+
 
